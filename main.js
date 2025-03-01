edd36e6ce2a00e9b902bb93adebf8ea9730c239a
VargaDoro <varga.dorotea.timea@gmail.com>
2025-03-01 14:47:35 +0100

    kész

--- main.js
+++ main.js
@@ -2,15 +2,21 @@ import { masodfokuTeszt } from "./teszt.js"
 
 export function masodfoku(a,b,c){
     let d = b*b-4*a*c
-    if (a=0){
-        return [-b/c]
+    if (a === 0) {  // Ha a = 0, akkor lineáris egyenlet
+        if (b === 0) {
+            return [NaN,NaN]; // Lineáris megoldás
+        }
+        return [-c/b];
     }
     if (d<0){
         return ["Nincs megoldás","Nincs megoldás"]
     }
     let x1 = (-b+Math.sqrt(b*b-4*a*c))/(2*a)
     let x2 = (-b-Math.sqrt(b*b-4*a*c))/(2*a)
-    return [x1, x2]
+    if (d === 0) {
+        return [x1, x1];  // Ha D = 0, egy gyök van
+    }
+    return [x1, x2];  // Ha D > 0, két gyök van
 }
 
 masodfoku(1,2,1)
--- teszt.js
+++ teszt.js
@@ -1,86 +1,80 @@
-import { masodfoku } from "./main.js"
+import { masodfoku } from "./main.js";
+
 export function masodfokuTeszt() {
-    const TESZTESETEKLISTA =[
+    const TESZTESETEKLISTA = [
         {
-            a:1,
-            b:2,
-            c:1,
-            vart:[-1, -1],
-            nev:"Pontosan 1 gyöke van",
+            a: 1,
+            b: 2,
+            c: 1,
+            vart: [-1, -1],
+            nev: "Pontosan 1 gyöke van",
         },
         {
             a: 1,
             b: 2,
             c: 5,
-            vart: ["Nincs megoldás","Nincs megoldás"],
-            nev:"Diszkrimuináns negatív, nincs megoldás",
+            vart: ["Nincs megoldás", "Nincs megoldás"],
+            nev: "Diszkrimináns negatív, nincs megoldás",
         },
         {
             a: 3,
             b: -9,
             c: 0,
             vart: [3, 0],
-            nev:"2 gyöke van",
+            nev: "2 gyöke van",
         },
         {
             a: 2,
             b: 5,
             c: 2,
             vart: [-0.5, -2],
-            nev:"2 negatív gyöke van",
+            nev: "2 negatív gyöke van",
         },
         {
             a: 10,
             b: 100,
             c: 1,
             vart: [-0.010010020050140156, -9.98998997994986],
-            nev:"2 negatív tört gyöke van",
+            nev: "2 negatív tört gyöke van",
         },
         {
             a: 1,
             b: 0,
             c: 0,
             vart: [0, 0],
-            nev:"b és a c nulla",
+            nev: "b és c nulla",
         },
         {
             a: 0,
             b: 1,
             c: 0,
-            vart: [NaN, -Infinity],
-            nev:"a és a c 0",
+            vart: [0],
+            nev: "Lineáris egyenlet, b = 1, c = 0",
         },
         {
             a: 0,
             b: 0,
             c: 1,
             vart: [NaN, NaN],
-            nev:"a és a b 0",
+            nev: "Lineáris egyenlet, b = 0, c = 1",
         },
         {
             a: 0,
             b: 0,
             c: 0,
             vart: [NaN, NaN],
-            nev:"mind 0",
+            nev: "Lineáris egyenlet, b = 0, c = 0",
         },
-    ]
-    let a;
-    let b;
-    let c;
-    let vart;
-    let nev; 
-    for (let index = 0; index < TESZTESETEKLISTA.length; index++){
-        a = TESZTESETEKLISTA[index].a;
-        b = TESZTESETEKLISTA[index].b;
-        c = TESZTESETEKLISTA[index].c
-        vart = TESZTESETEKLISTA[index].vart
-        nev = TESZTESETEKLISTA[index].nev
-        let kapott = masodfoku(a,b,c)
-        //console.log(`a: ${a}, b: ${b}, c: ${c} várt: ${vart} kapott: ${kapott} jó? ${vart===kapott}`);
+    ];
+
+    for (let index = 0; index < TESZTESETEKLISTA.length; index++) {
+        const { a, b, c, vart, nev } = TESZTESETEKLISTA[index];
+        const kapott = masodfoku(a, b, c);
+        const areNaN = (val1, val2) => isNaN(val1) && isNaN(val2);
         console.assert(
-            vart[0] === kapott[0] && vart[1] === kapott[1],
-            ` ${TESZTESETEKLISTA[index].nev} a: ${a}, b: ${b}, c: ${c} várt: ${vart} kapott: ${kapott} jó? ${vart===kapott}`
+            (areNaN(vart[0], kapott[0]) && areNaN(vart[1], kapott[1])) ||
+            (vart[0] === kapott[0] && vart[1] === kapott[1]),
+            `${nev} a: ${a}, b: ${b}, c: ${c} várt: ${vart} kapott: ${kapott}`
         );
     }
 }
